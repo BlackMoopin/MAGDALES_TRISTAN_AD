b7327755b92f28b66171bc18adce296bfe586a47
Tristan Josh Magdales <magdalestj@gmail.com>
2021-09-07 21:23:57 +0800

    Final

---
+++ 2_JS/VariableTypes.js
@@ -0,0 +1,17 @@
+var integer = 25;
+var word = 'Hello!';
+var check = true;
+var undefVar;
+var array = ['Mershed Pederder', 'Gurps', 'Blanaba'];
+var empty = null;
+
+
+console.log("The variable " + integer + " is of type " + typeof integer);
+console.log("The variable " + word + " is of type " + typeof word);
+console.log("The variable " + check + " is of type " + typeof check);
+console.log("The variable " + undefVar + " is of type " + typeof undefVar);
+console.log("The variable " + array + " is of type " + typeof array);
+console.log();
+console.log("The variable " + empty + " is of type " + typeof empty);
+console.log("Variable with value of null will always be object"); //from what I researched it is not "null" for legacy reasons
+console.log();
