a2666f0ccaa0fae3431b366bfc64ee954dd692f3
Tristan Josh Magdales <magdalestj@gmail.com>
2021-09-10 12:24:32 +0800

    3-nest Assignment

--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class Exercise3Service {
-    helloWorld(){
-        console.log("Hilu Goodmorneng");
-        return "Hilu Goodmorneng";
+    hello(name: string){
+        return "Hilu Goodmorneng " + name + "!";
     }
 
     loopsTriangle(height:number){
@@ -14,16 +13,15 @@ export class Exercise3Service {
             string += '*';
             console.log(string);
         }
-
         return;
     }
 
-    primeNumber(num:number):string{
-        var isPrime = 0;
+    prime(num:number):string{
+        var isComposite = 0;
 
         for (var i = 2; i < num / 2; i++) {
             if (num % i == 0) {
-                isPrime = 1;
+                isComposite = 1;
                 break;
             }
         }
@@ -31,9 +29,9 @@ export class Exercise3Service {
         if (num < 1) {
             return ("Please enter a number greater than 0.");
         } else if (num == 1) {
-            return (num + " is a not a prime number.");
+            return (num + " is a neither a prime nor a composite number.");
         } else {
-            if (isPrime == 1) {
+            if (isComposite == 1) {
                 return (num + " is a not a prime number.");
             } else {
                 return (num + " is a prime  number.");
