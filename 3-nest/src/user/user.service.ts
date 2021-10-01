d1da02efc15c0c412588b91fcf7e3fdc350bc469
Tristan Josh Magdales <magdalestj@gmail.com>
2021-10-01 15:18:03 +0800

    Final(?)

--- 3-nest/src/user/user.service.ts
+++ 3-nest/src/user/user.service.ts
@@ -1,84 +1,203 @@
 import { All, Injectable } from '@nestjs/common';
+import { CRUDReturn } from './crud_return.interface';
+import { Helper } from './user.resource/helper';
 import { User } from './user.model';
+
+const DEBUG: boolean = true;
 
 @Injectable()
 export class UserService {
 
     private users: Map<string, User> = new Map<string, User>();
+    email:string;
+    password: string;
+    age:number;
+    name: string;
 
-    addUser(user:any){
-        var newUser: User;
-        newUser = new User(user?.id, user?.name, user?.age, user?.email, user?.password);
-        this.users.set(user.id, newUser);
-        return true;
-    }
-
-    replaceUser(user:any, id:string){
-        var newUser: User;
-        newUser = new User(id, user?.name, user?.age, user?.email, user?.password);
-        this.users.set(id, newUser);
-        return true;
-    }
-
-    updateUser(user:any, id:string){
-        var newUser:User = this.users.get(id);
-        if(newUser.update(user)){
-            return true;
-        } else {
-            return false;
+    //post - finished
+    addUser(user:any): CRUDReturn{
+        try {
+            var validBody: {valid: boolean; data: string} = Helper.validBody(user);
+            if (validBody.valid){
+                if (!this.checkExistingUsers(user.email)){
+                    var newUser: User = new User(
+                        user.name,
+                        user.age,
+                        user.email,
+                        user.password,
+                    );
+                    if (this.saveToDB(newUser)){
+                        if (DEBUG) this.userLog();
+                        return {
+                            success: true,
+                            data: newUser.toJson(),
+                        };
+                    } else {
+                        throw new Error(`Generic Database Error`);
+                    }
+                } else {
+                    throw new Error(`${user.email} is already used by another user!`)
+                }
+                
+            } else {
+                throw new Error(validBody.data);
+            }
+        } catch (error) {
+            console.log(error);
+            return { success: false, data: `Error adding account, ${error.message}`};
         }
         
     }
 
-    userLog(){
-        var users = [];
-        for (const [key, user] of this.users.entries()){
-            users.push(user);
-        }
-        console.log(users);
-        return users;
-    }
-
-    getUser(id:string){
-        if (this.users.has(id)){
-            return this.users.get(id).toJson();
-        } else {
-            console.log(id + " does not exist in the database!");
-            return id + " does not exist in the database!";
-        }
-    }
-
-    deleteUser(id:string){
-        if (this.users.has(id)){
-            this.users.delete(id);
-        } else {
-            console.log(id + " does not exist in the database!");
-            return id + " does not exist in the database!";
-        }
-    }
-
-    check(email: string, password: string){
-        var check = 0;
-        for (const [key, user] of this.users.entries()){
-           if(user.login(email, password)){
-               check = 1;
-               break;
+    //put - finished
+    replaceUser(user:any, id:string): CRUDReturn{
+        try {
+            var newUser:User = this.users.get(id);
+            if(newUser.replaceAllValues(user)){
+                return {
+                    success: true,
+                    data: newUser.toJson(),
+                };
             } else {
-                check = 0;
+                return {
+                    success: false,
+                    data: `Invalid or insufficient input(s) is entered.`,
+                };
             }
+        } catch (error) {
+            return { success: false, data: `Error replacing account, ${error.message}`};
+        }      
+    }
+
+    //patch - finished
+    updateUser(user:any, id:string) : CRUDReturn{
+        try {
+            var validBody: {valid: boolean; data: string} = Helper.validBody(user);
+            if (validBody.valid){
+                var newUser:User = this.users.get(id);
+                if(newUser.update(user)){
+                    return {
+                        success: true,
+                        data: newUser.toJson(),
+                    };
+                } else {
+                    return {
+                        success: false,
+                        data: `Invalid Input is entered.`,
+                    };
+                }
+            } else {
+                throw new Error(validBody.data);
+            }
+        } catch (error) {
+            return { success: false, data: `Error updating account, ${error.message}`};
         }
-        if (check === 1){
-            return "Welcome! " + email;
+        
+    }
+
+    //finish
+    userLog(): CRUDReturn {
+        var results: Array<any> = [];
+        for (const user of this.users.values()){
+            results.push(user.toJson());
+            console.log(results);
+        }
+        return { success: results.length>0, data: results};
+    }
+
+    //finish
+    getUser(id:string) : CRUDReturn{
+        if (this.users.has(id)){
+            return { success: true, data: this.users.get(id).toJson()};
         } else {
-            return "Incorrect Credentials!";
+            console.log(id + " does not exist in the database!");
+            return { 
+                success: false, 
+                data: `User ${id} is not in the database`, 
+            };
         }
     }
 
-    searchUser(term:any){
-        for (const [key, user] of this.users.entries()) {
-            if (user.search(term) === true){
-                return user.toJson();
+    //finish
+    deleteUser(id:string){
+        try {
+            if (this.users.has(id)){
+                this.users.delete(id);
+                return {
+                    success: true,
+                    data: `User has been deleted succesfully`,
+                };
             }
+        } catch (error) {
+            return { 
+                success: false, 
+                data: `User ${id} is not in the database`, 
+            };
+        }
+    }
+
+    //finish
+    check(email: string, password: string): CRUDReturn{
+        var check: number = 0;
+        try {
+            for (const user of this.users.values()){
+                if (user.matches(email)){
+                    if(user.login(password)){
+                        return user.login(password);
+                        check = 1;
+                        break;
+                    } else {
+                        throw new Error(`Invalid Password for ${email}!`);
+                    }
+                } 
+            }
+
+            if (check === 0){
+                return { 
+                    success: false, 
+                    data: `Email ${email} is not in the database`, 
+                };
+            }
+        } catch (error) {
+            return { 
+                success: false, 
+                data: `Error logging in, ${error.message}`, 
+            };
+        }
+        
+    }
+
+    //finish
+    searchUser(term:string): CRUDReturn{
+        var results: Array<any> = [];
+        for (const user of this.users.values()) {
+            if (user.matches(term)){
+                results.push(user.toJson());
+            }
+        }
+        return {
+            success: results.length>0,
+            data: results,
+        };
+    }
+
+    //finish
+    checkExistingUsers(term:string): boolean{
+        for (const user of this.users.values()) {
+            if (user.search(term) === true){
+                return true;
+            }
+        }
+    }
+
+    //finish
+    saveToDB(user: User): boolean{
+        try {
+            this.users.set(user.id, user);
+            return this.users.has(user.id);
+        } catch (error) {
+            console.log(error);
+            return false;
         }
     }
 
